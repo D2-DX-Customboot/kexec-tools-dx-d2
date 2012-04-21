f61ccc57e45ff5c0587e99c83994023dc1d57d71
Matt Parnell <parwok@gmail.com>
2012-04-20 17:09:32 -0700

    temporarily? disable ifdown, which allows for building for arm

--- kexec/ifdown.c
+++ kexec/ifdown.c
@@ -27,48 +27,5 @@ char *v_ifdown = "@(#)ifdown.c  1.11  02-Jun-1998  miquels@cistron.nl";
  */
 int ifdown(void)
 {
-	struct if_nameindex *ifa, *ifp;
-	struct ifreq ifr;
-	int fd, shaper;
-
-	if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-		fprintf(stderr, "ifdown: ");
-		perror("socket");
-		return -1;
-	}
-
-	if ((ifa = if_nameindex()) == NULL) {
-		fprintf(stderr, "ifdown: ");
-		perror("if_nameindex");
-		return -1;
-	}
-
-	for (shaper = 1; shaper >= 0; shaper--) {
-		for (ifp = ifa; ifp->if_index; ifp++) {
-
-			if ((strncmp(ifp->if_name, "shaper", 6) == 0)
-			    != shaper) continue;
-			if (strcmp(ifp->if_name, "lo") == 0)
-				continue;
-			if (strchr(ifp->if_name, ':') != NULL)
-				continue;
-
-			strncpy(ifr.ifr_name, ifp->if_name, IFNAMSIZ);
-			if (ioctl(fd, SIOCGIFFLAGS, &ifr) < 0) {
-				fprintf(stderr, "ifdown: shutdown ");
-				perror(ifp->if_name);
-				return -1;
-			}
-			ifr.ifr_flags &= ~(IFF_UP);
-			if (ioctl(fd, SIOCSIFFLAGS, &ifr) < 0) {
-				fprintf(stderr, "ifdown: shutdown ");
-				perror(ifp->if_name);
-				return -1;
-			}
-
-		}
-	}
-	close(fd);
-
 	return 0;
 }
